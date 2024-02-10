123e7919e145f7ce8c8f9190e9cd8041ae7de75c
Joe Harris <joeharris04200@gmail.com>
2024-02-10 10:11:07 -0700

    Changed an href on the about page

--- src/components/About.js
+++ src/components/About.js
@@ -21,7 +21,7 @@ const About = () => {
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
             unde dolor?
           </p>
-          <a href="localhost3000/#" className="btn">
+          <a href="#about" className="btn">
             read more
           </a>
         </article>
